eefc6fa9560145e91fdcc7642da915cf574640d2
Phil <batteryphil@local>
2026-06-10 11:02:07 -0500

    self-evolve: Implement a thin wrapper around networkx to easily get neigh

--- Helix-AGI/tools/networkx_wrapper.py
+++ Helix-AGI/tools/networkx_wrapper.py
@@ -1,17 +1,18 @@
+import json
 import networkx as nx
 
-def shortest_path(digraph, node1, node2):
-    return nx.shortest_path(digraph, source=node1, target=node2)
+def get_neighbors(digraph, node):
+    return list(digraph.neighbors(node))
+
+def main():
+    with open('graph.jsonl', 'r') as f:
+        graph_data = [json.loads(line) for line in f]
+    graph = nx.DiGraph()
+    for data in graph_data:
+        graph.add_edge(data['source'], data['target'])
+    node = 'A'
+    neighbors = get_neighbors(graph, node)
+    print(f"Neighbors of {node}: {neighbors}")
 
 if __name__ == '__main__':
-    G = nx.DiGraph()
-    G.add_edge('A', 'B', weight=5)
-    G.add_edge('B', 'C', weight=4)
-    G.add_edge('A', 'D', weight=2)
-    G.add_edge('D', 'C', weight=1)
-    G.add_edge('D', 'E', weight=3)
-    G.add_edge('E', 'C', weight=6)
-
-    print(shortest_path(G, 'A', 'C'))  # ['A', 'D', 'C']
-    print(shortest_path(G, 'A', 'E'))  # ['A', 'D', 'E']
-    print(shortest_path(G, 'B', 'E'))  # []
+    main()
