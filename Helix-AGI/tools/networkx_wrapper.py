6178eebf2ef2ea27fae3c24dbf7ebd218878e346
Phil <batteryphil@local>
2026-06-10 21:35:35 -0500

    self-evolve: Implement a thin wrapper around networkx that allows retriev

--- Helix-AGI/tools/networkx_wrapper.py
+++ Helix-AGI/tools/networkx_wrapper.py
@@ -1,25 +1,17 @@
-import json
-import jsonlines
 import networkx as nx
 
-def load_jsonl_to_digraph(file_path):
-    G = nx.DiGraph()
-    with jsonlines.open(file_path) as f:
-        for line in f:
-            data = json.loads(line)
-            node = data['node']
-            neighbors = data.get('neighbors', [])
-            G.add_node(node)
-            for neighbor in neighbors:
-                G.add_edge(node, neighbor)
-    return G
+class NetworkXWrapper:
+    def __init__(self, graph):
+        self.graph = graph
 
-def get_neighbors(G, node):
-    return list(G.successors(node))
+    def get_neighbors(self, node):
+        return list(self.graph.neighbors(node))
+
+def main():
+    G = nx.DiGraph()
+    G.add_edges_from([(1, 2), (2, 3), (3, 1), (3, 4), (4, 2)])
+    wrapper = NetworkXWrapper(G)
+    print(wrapper.get_neighbors(3))
 
 if __name__ == '__main__':
-    file_path = 'path/to/jsonl/file'
-    G = load_jsonl_to_digraph(file_path)
-    node = 'example_node'
-    neighbors = get_neighbors(G, node)
-    print(f"Neighbors of {node}: {neighbors}")
+    main()
