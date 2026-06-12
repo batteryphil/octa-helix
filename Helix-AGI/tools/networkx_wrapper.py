a130ac06b03345f191334773a37a74ee14aa84dd
Phil <batteryphil@local>
2026-06-12 15:08:36 -0500

    self-evolve: Add a thin wrapper to load a JSONL file as a networkx DiGrap

--- Helix-AGI/tools/networkx_wrapper.py
+++ Helix-AGI/tools/networkx_wrapper.py
@@ -2,27 +2,20 @@ import json
 import jsonlines
 import networkx as nx
 
-class NetworkxWrapper:
-    @staticmethod
-    def from_jsonl(file_path):
-        G = nx.DiGraph()
-        with jsonlines.open(file_path) as f:
-            for line in f:
-                data = json.loads(line)
-                node = data['node']
-                neighbors = data.get('neighbors', [])
-                G.add_node(node)
-                for neighbor in neighbors:
-                    G.add_edge(node, neighbor)
-        return G
+def load_jsonl_as_digraph(file_path):
+    G = nx.DiGraph()
+    with jsonlines.open(file_path) as f:
+        for line in f:
+            data = json.loads(line)
+            G.add_edge(data['source'], data['target'])
+    return G
 
-    @staticmethod
-    def get_neighbors(G, node):
-        return list(G.successors(node))
+def get_neighbors(G, node):
+    return list(G.neighbors(node))
 
 if __name__ == '__main__':
     file_path = 'example.jsonl'
-    G = NetworkxWrapper.from_jsonl(file_path)
-    node = 'A'
-    neighbors = NetworkxWrapper.get_neighbors(G, node)
-    print(f"Neighbors of {node}: {neighbors}")
+    node = 'example_node'
+    G = load_jsonl_as_digraph(file_path)
+    neighbors = get_neighbors(G, node)
+    print(neighbors)
